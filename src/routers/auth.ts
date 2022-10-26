72f520155ef7df0a30b8e4676004bdae67ecd1ae
AndrosovMA <AndrosovMA@bk.ru>
2022-10-26 15:23:25 +0300

    fix bug:  in auth

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -4,13 +4,25 @@ import {checkResultErrorsMiddleware, loginValidation, passwordValidation} from "
 
 export const auth = Router();
 
-auth.post('/login', loginValidation, passwordValidation,
-    checkResultErrorsMiddleware,
+auth.post('/login', //loginValidation, passwordValidation,
+   // checkResultErrorsMiddleware,
 
     async (req: Request, res: Response) => {
 
         const login = req.body.login;
         const password = req.body.password;
+
+        if(login === undefined) {
+            return res.status(400).send({
+                errorsMessages: [{message: 'Invalid login', field: 'login'}]
+            })
+        }
+
+        if(password === undefined) {
+            return res.status(400).send({
+                errorsMessages: [{message: 'Invalid password', field: 'password'}]
+            })
+        }
 
         const isCheckCredentials = await usersService.checkCredentials(login, password);
 
